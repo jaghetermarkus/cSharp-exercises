807837ae79f47a7115490bd11a4084a4622536b2
jaghetermarkus <143387643+jaghetermarkus@users.noreply.github.com>
2023-09-04 11:21:18 +0200

    Adding folders in Lektion-3.

---
+++ Lektion-3/02_NamingConventions/Program.cs
@@ -0,0 +1,21 @@
+﻿
+/*
+
+DESIGN PATTERN vid namngivning
+-------------------------------------------------------------------------
+camelCase       firstName           Javascript, Java, C/C++, (C#)
+Pascal          FirstName           C#, Pascal, Turbo Pascal
+snake_case      first_name          Python, C/C++
+kebab-case      first-name          PowerShell, CSS
+
+Variabler           string firstName
+Privata variabler   private string _firstName
+Parameter           string firstName    MethodName(string firstname, string lastname) {}
+
+
+Class - Field           firstName       private string _firstName;
+Class - Property        FirstName       public string FirstName { get; set; }
+Class - Methods         MethodName      public void MethodName() {}
+
+
+*/
---
+++ Lektion-3/03_LoopsAndConditions/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+
+IF STATEMENT (Operander)
+------------------------------------------------------------------------
+==          lika med
+!=          inte lika med
+<           mindre än
+<=          mindre eller lika med
+>           större än
+>=          större eller lika med
+&&          och
+||          eller           (mac: opt + 7)
+!           om det inte är det här
+??          om första värdet är null -> välj det andra
+
+
+**** OLIKA SÄTT ATT SKRIVA IF-SATSER PÅ ****
+1.  if (true) {}
+
+2.  if (true) {} else {}
+
+3a. if (true) {} else if (true) {} else {}
+
+3b. if (true) {} else if (true) {} else if (true) {} else if (true) {} else {}
+
+4.  (true) ? do_this : do_this;
+
+
+string name = "Markus";
+string firstName = "Vidar";
+
+if (name == firstName)
+{
+    Console.WriteLine("Namnen är lika.");
+}
+else
+{
+    Console.WriteLine("Namnen är inte lika.");
+}
+
+
+string firstName = "";
+
+if (!string.IsNullOrEmpty(firstName))
+{
+    Console.WriteLine("Nu gör vi någonting med förnamnet");
+}
+else
+{
+    Console.WriteLine("Det finns inget förnamn");
+}
+
+*/
+
+
+string firstName = "Markus";
+string lastName = "";
+
+if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+{
+    Console.WriteLine("Något utan värdena är inte ifyllda.");
+}
+
+Console.ReadKey();
+
+
